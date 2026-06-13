4222c915ebe89b8eed8c1c9f8cec2418a96fc64b
nwmgraspr <rmblamah@gmail.com>
2026-06-13 01:33:35 -0500

    Update case_restaurant_query_order_aggregate.sql

--- sql/sqlite/case_restaurant_query_order_aggregate.sql
+++ sql/sqlite/case_restaurant_query_order_aggregate.sql
@@ -1,8 +1,8 @@
--- sql/sqlite/case_retail_query_sales_aggregate.sql
+-- sql/sqlite/case_restaurant_query_orders_aggregate.sql
 -- ============================================================
 -- PURPOSE
 -- ============================================================
--- Summarize overall sales activity across ALL stores.
+-- Summarize overall orders activity across ALL stores.
 --
 -- This query answers:
 -- - "What is our total revenue?"
@@ -15,7 +15,7 @@
 --   "Is overall performance up or down?"
 
 SELECT
-  COUNT(*) AS sale_count,
+  COUNT(*) AS order_count,
   ROUND(SUM(amount), 2) AS total_revenue,
-  ROUND(AVG(amount), 2) AS avg_sale_amount
+  ROUND(AVG(amount), 2) AS avg_order_amount
 FROM sale;
